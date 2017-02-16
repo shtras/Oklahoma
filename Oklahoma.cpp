c651f08fbe8047b5ae3be4b525fcc35d90b41748
Gregory Shtrasberg <shtras@gmail.com>
2017-02-17 00:33:03 +0200

    FPS calculation

--- Oklahoma.cpp
+++ Oklahoma.cpp
@@ -34,6 +34,11 @@ Oklahoma& Oklahoma::GetInstance()
 
 void Oklahoma::Run()
 {
+    int lastTicks = SDL_GetTicks();
+    int fpsTickBase = lastTicks;
+    int frames = 0;
+    wchar_t fpsStr[128];
+
     while (running_) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
@@ -44,17 +49,27 @@ void Oklahoma::Run()
                 break;
             }
         }
+
+        int currTicks = SDL_GetTicks();
+        int deltaTicks = currTicks - lastTicks;
+        ++frames;
+        if (deltaTicks > 100) {
+            lastTicks = currTicks;
+            float fps = frames / (float)deltaTicks * 1000.0f;
+            frames = 0;
+            swprintf_s(fpsStr, L"FPS: %.2f", fps);
+        }
+
         renderer_->StartFrame();
 
         //renderer_->SetTexture(Renderer::TEX_GUI);
         //renderer_->RenderRect(Rect({ 0.0f, 0.0f, 0.5f, 0.5f }), Rect({ 0.0f, 0.0f, 1.0f, 1.0f }));
         //renderer_->RenderRect(Rect({ 0.5f, 0.0f, 0.3f, 0.3f }), Rect({ 0.0f, 0.0f, 0.5f, 0.5f }));
-
         renderer_->RenderText(L"abcdefghijklmnopqrstuvwxyz", 0, 0);
         renderer_->RenderText(L"ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0, 0.3f);
         renderer_->RenderText(L"0123456789/", 0, 0.5f);
         renderer_->RenderText(L"!@#$%^&*()-=_+,.;:'\"[]{}\\|", 0, 0.7f);
-
+        renderer_->RenderText(fpsStr, 0, 0.9f);
         renderer_->RenderFrame();
     }
 }
@@ -63,6 +78,7 @@ void Oklahoma::Init()
 {
     renderer_ = new Renderer();
     renderer_->Init();
+    renderer_->SetCharSize(20, 40);
 }
 
 Oklahoma::Oklahoma() :
