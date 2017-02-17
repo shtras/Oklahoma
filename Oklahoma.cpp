da3c6c037b2c691e41096c7e0cbb38aab10b7021
Gregory Shtrasberg <shtras@gmail.com>
2017-02-17 14:53:03 +0200

    Buildability

--- Oklahoma.cpp
+++ Oklahoma.cpp
@@ -3,7 +3,6 @@
 #include "Version.h"
 
 using namespace OGraphics;
-using namespace OGUI;
 
 int __stdcall WinMain(__in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, __in LPSTR lpCmdLine, __in int nShowCmd)
 {
